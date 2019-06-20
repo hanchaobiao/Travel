6a3530f0a1cdeac13c60b7edce4c6cd0660083a4
hancb <1017421922@qq.com>
2019-06-20 23:04:00 +0800

    添加查看更多

--- src/pages/Home/components/Recommend.vue
+++ src/pages/Home/components/Recommend.vue
@@ -11,6 +11,7 @@
         </div>
       </li>
     </ul>
+    <div class="more">查看所有产品</div>
   </div>
 </template>
 
@@ -66,35 +67,41 @@ export default {
   @import '~styles/mixins.styl'
   .recommend-container
     background: #fff
-  .title
-    /*margin-top: .2rem*/
-    line-height: .8rem
-    background: #eee
-    text-indent: .2rem  // 文字缩进
-    @ellipsis()
-  .item
-    overflow: hidden
-    display: flex
-    height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.7rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      padding: .1rem
-      .item-title
-        line-height: .54rem
-        font-size: .32rem
-        @ellipsis()
-      .item-desc
-        line-height: .4rem
-        color: #ccc
-        @ellipsis()
-      .item-button
-        line-height: .44rem
-        margin-top: .2rem
-        background: #ff9300
-        padding: 0 .2rem
-        border-radius: .06rem
+    .title
+      /*margin-top: .2rem*/
+      line-height: .8rem
+      background: #eee
+      text-indent: .2rem  // 文字缩进
+      @ellipsis()
+    .item
+      overflow: hidden
+      display: flex
+      height: 1.9rem
+      .item-img
+        width: 1.7rem
+        height: 1.7rem
+        padding: .1rem
+      .item-info
+        flex: 1
+        padding: .1rem
+        .item-title
+          line-height: .54rem
+          font-size: .32rem
+          @ellipsis()
+        .item-desc
+          line-height: .4rem
+          color: #ccc
+          @ellipsis()
+        .item-button
+          line-height: .44rem
+          margin-top: .2rem
+          background: #ff9300
+          padding: 0 .2rem
+          border-radius: .06rem
+    .more
+      height: 1rem
+      line-height: 1rem
+      text-align: center
+      color: #00afc7
+      font-size: .28rem
 </style>
