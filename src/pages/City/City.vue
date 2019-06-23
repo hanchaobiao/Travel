d3fa77971480c511023ddaca31079f1b48f7d8b6
hancb <1017421922@qq.com>
2019-06-23 19:50:11 +0800

    1、城市选择 添加search样式

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,22 +1,60 @@
 <template>
-  <div>
-  <city-header></city-header>
-  <keep-alive>
-    <router-view></router-view>
-  </keep-alive>
+  <div class="city-container">
+    <city-header></city-header>
+    <div class="tab-wrapper">
+      <ul class="tabs">
+        <li :class="{ active: isActive==1 }" class="tab-item" @click="cityType(1)">境内</li>
+        <li :class="{ active: isActive==2 }" class="tab-item" @click="cityType(2)">国际城市</li>
+      </ul>
+    </div>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
+  },
+  data () {
+    return {
+      isActive: 1
+    }
+  },
+  methods: {
+    cityType (index) {
+      this.isActive = index
+    }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .tab-wrapper
+    width: 100%
+    background $bgColor
+    height: .6rem
+    font-size: .28rem
+    .tabs
+      padding: 0 1rem
+      height: .4rem
+      line-height: .4rem
+      list-style: none
+      .tab-item
+        box-sizing: border-box
+        float: left
+        width: 50%
+        height: 100%
+        border: .02rem solid #fff
+        text-align: center
+        color: #fff
+      .active
+        background: #fff
+        color: $bgColor
 </style>
--- src/pages/City/components/Header.vue
+++ src/pages/City/components/Header.vue
@@ -4,16 +4,6 @@
       <div class="iconfont header-back">&#xe624;</div>
       城市选择
     </div>
-    <div class="tab-wrapper">
-      <ul class="tabs">
-        <router-link to="/">
-          <li class="tab-item active">境内</li>
-        </router-link>
-        <router-link to="/">
-          <li class="tab-item">国际城市</li>
-        </router-link>
-      </ul>
-    </div>
   </div>
 </template>
 
@@ -41,23 +31,4 @@ export default {
         width: .64rem
         text-align: center
         font-size: .4rem
-    .tab-wrapper
-      height: .8rem
-      font-size: .28rem
-      .tabs
-        padding: .2rem 1rem
-        height: .4rem
-        line-height: .4rem
-        list-style: none
-        .tab-item
-          box-sizing: border-box
-          float: left
-          width: 50%
-          height: 100%
-          border: .02rem solid #fff
-          text-align: center
-          color: #fff
-        .active
-          background: #fff
-          color: $bgColor
 </style>
